f52ee26dbc6fb4f49f0763f01d7682926dbb648c
Raymond Nehring <rnehring89@gmail.com>
2022-03-03 11:24:00 -0600

    Working on inputs

--- src/App.js
+++ src/App.js
@@ -5,58 +5,70 @@ import scrambler from "./scrambler";
 import {rowCount, columnCount} from "./sentenceParser"
 
 function App() {
-  const [data, setData] = useState("")
-  const [loading, setLoading] = useState(true)
-  const [word, setWord] = useState('')
-  let newNum = useRef(1)
-  const score = useRef(0)
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(true);
+  let newNum = useRef(1);
+  const score = useRef(0);
   const savedWord = useRef();
+  const inputRef = useRef(null);
 
   //Counter passed into fetch API.
   let number = 1;
 
+   const setFocus = () => {
+     //Add id to the table
+     let first = document.getElementsByTagName("input")[0];
+
+     //table > tbody > tr (latest added row) > td (first cell in the row) > input
+     //  let cell =
+     //    document.getElementById("mytable").lastElementChild.lastElementChild
+     //      .firstElementChild.children[0];
+     console.log(first);
+     if (first) {
+       first.focus();
+     }
+   };
+   setFocus()
 
   //Fetching data and assigning to setData useState.
   useEffect(() => {
     axios(`https://api.hatchways.io/assessment/sentences/${newNum.current}`)
-    .then(response => {
-      setData(response.data.data)
-    })
-    .catch(error => {console.log("error fetching data")})
-    .finally(setLoading(false))
-  }, [])
-
+      .then((response) => {
+        setData(response.data.data);
+      })
+      .catch((error) => {
+        console.log("error fetching data");
+      })
+      .finally(setLoading(false));
+  }, []);
 
   //Splitting the string received from fetch, keeping the space.
-  let scrambled;
   let newWord;
-  if(data){
-    scrambled = scrambler(data.sentence);
-    savedWord.current = scrambled;
-    newWord = (
-    scrambled.split(/(\S+\s+)/).filter(function (n) {
+  if (data) {
+    newWord = data.sentence.split(/(\S+\s+)/).filter(function (n) {
       return n;
-    })
-    );
+    });
   }
+  let counter = 0;
+
+  
+
+   
 
   return (
     <div className="main-container">
       <div className="inner-container">
         <div className="sentence-and-instructions">
-          <h1 className="sentence">{data && scrambled}</h1>
+          <h1 className="sentence">{data && scrambler(data.sentence)}</h1>
           <span className="instructions">Guess the sentence! Start typing</span>
           <span className="instructions">
             The yellow blocks are meant for spaces
           </span>
         </div>
-        <div>
-          Score: {score.current}
-        </div>
+        <div>Score: {score.current}</div>
         <div className="guess-container">
           {data &&
             newWord.map((el) => {
-              
               return (
                 <div className="word">
                   {el &&
@@ -64,14 +76,20 @@ function App() {
                       return lett == " " ? (
                         <input
                           className="space"
-                          onChange={(e) => setWord(e.target.value)}
-                          // value={lett}
+                          key={(counter += 1)}
+                          maxLength="1"
+                          onChange={(e) =>
+                            e.target.value == lett ? console.log("YES") : "no"
+                          }
                         ></input>
                       ) : (
                         <input
+                          key={(counter += 1)}
                           className="letter"
-                          onChange={(e) => e.target.value == lett ? (console.log("yes")) : "no"}
-                          value={''}
+                          maxLength="1"
+                          onChange={(e) =>
+                            e.target.value == lett ? console.log("YES") : "no"
+                          }
                         ></input>
                       );
                     })}
